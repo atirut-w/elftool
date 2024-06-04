6c7895dfe83c428e42b370388790ba3b1326c7f7
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-06-04 15:03:00 +0700

    Loader stubs

--- include/elftool/elf.hpp
+++ include/elftool/elf.hpp
@@ -1,1 +1,11 @@
 #pragma once
+#include <istream>
+
+namespace ELFTool
+{
+    class ELF
+    {
+    public:
+        ELF(std::istream& stream);
+    };
+}
--- src/elf.cpp
+++ src/elf.cpp
@@ -1,1 +1,22 @@
 #include <elftool/elf.hpp>
+#include <stdexcept>
+#include <string>
+
+using namespace std;
+using namespace ELFTool;
+
+ELF::ELF(istream& stream)
+{
+    if (!stream)
+    {
+        throw invalid_argument("invalid input stream");
+    }
+
+    string magic;
+    magic.resize(4);
+    stream.read(&magic[0], 4);
+    if (magic != "\x7f""ELF")
+    {
+        throw invalid_argument("invalid ELF magic");
+    }
+}
